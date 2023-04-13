e332da9aaf90fe77e76bd1e2d6c206e54604cef7
lookfrank <129953124+lookfrank@users.noreply.github.com>
2023-04-13 15:56:34 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/app";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请在设置页输入授权码。",
+    Unauthorized: "现在是未授权状态，关注公众号“AI产品万花筒”免费获取授权码",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
